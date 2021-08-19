252ecd2205fd6a65172f70aaf1669a1f0d2a3d43
aakar-gupta-1 <aakar.gupta2019@vitstudent.ac.in>
2021-08-19 18:37:49 +0530

    find duplicate number

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,27 @@
 package com.company;
 
-public class Main {
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int low=1,high=nums.length-1;
+        int dupli=-1;
 
-    public static void main(String[] args) {
-	// write your code here
-        System.out.println("aakar");
+        while(low<=high){
+            int cur = (low+high)/2;
+
+            int count=0;
+            for(int num : nums){
+                if(num <= cur) count++;
+            }
+
+            if(count>cur){
+                dupli = cur;
+                high=cur-1;
+            } else {
+                low=cur+1;
+            }
+
+        }
+
+        return dupli;
     }
 }
